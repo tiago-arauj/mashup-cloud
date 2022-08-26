023e78a95b439464e77e43bb42aac641148fb1f4
Tiago araujo <araujo.mtiago@gmail.com>
2022-08-26 08:28:20 -0300

    correcao connect

--- connect.js
+++ connect.js
@@ -4,9 +4,9 @@
     const WebSocket = require('ws');
   
     // replace with your information
-    const appId = '<fdf3fa37-f6b7-43e2-bb0f-6862124dee7b>';
-    const tenant = '<2x4essgkgudqooz.us.qlikcloud.com>';
-    const apiKey = '<XiLh3EgX-n7mt0wp9BVMMZp-yPafRcIPDU8c>';
+    const appId = 'fdf3fa37-f6b7-43e2-bb0f-6862124dee7b';
+    const tenant = '2x4essgkgudqooz.us.qlikcloud.com';
+    const apiKey = 'eyJhbGciOiJFUzM4NCIsImtpZCI6ImIxNzMyZDBlLTM1ODUtNDQxNi04NWU1LTE4NzgxNDYzNTc0MiIsInR5cCI6IkpXVCJ9.eyJzdWJUeXBlIjoidXNlciIsInRlbmFudElkIjoiR0Q2SW83TFMzODBBTFVhUVlmQVIxSnhHcXBBS0dFd3QiLCJqdGkiOiJiMTczMmQwZS0zNTg1LTQ0MTYtODVlNS0xODc4MTQ2MzU3NDIiLCJhdWQiOiJxbGlrLmFwaSIsImlzcyI6InFsaWsuYXBpL2FwaS1rZXlzIiwic3ViIjoiNjJkODUyYWI2Y2U1YzIwMjU1ZjkzYWIzIn0.3Qn5aTNwYLLxbztkIWDOGsiGjmIAQ8Y_9z55zBiRafQcKEJoKJH53h-R8-83zurSCXDqoVjHS3iwE6Sw1j1uKuv7D1IQztsgmlDwSKLElxfCMPNggXi6JHojVVhnsuQ4';
   
     const url = `wss://${tenant}/app/${appId}`;
   
@@ -30,6 +30,6 @@
       await session.close();
       console.log('Session closed!');
     } catch (err) {
-      console.log('Something went wrong :(', err);
+      console.log('Something went wrong :(', err.message);
     }
   })();
